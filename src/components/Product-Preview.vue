e2898b5f2e8a6ddb25f8199fa564a39250c707fc
@Cornelio <jun23.llagas@gmail.com>
2025-05-03 14:05:22 +0800

    fixing isssues for perfomance

--- src/components/Product-Preview.vue
+++ src/components/Product-Preview.vue
@@ -1,75 +1,79 @@
-<script>
-</script>
-
 <template>
-  <article>
+  <div class="product-container">
     <header>
       <picture>
-         <source srcset="/src/images/image-product-desktop.jpg" media="(min-width: 48em)"/>
-        <img src="/src/images/image-product-mobile.jpg" alt="Elegant parfume box"  loading="eager" decoding="async" />
+        <source srcset="/src/images/image-product-desktop.jpg" media="(min-width: 37.5rem)" />
+        <img src="/src/images/image-product-mobile.jpg" alt="Elegant parfume box" fetchpriority="high" decoding="async"/>
       </picture>
     </header>
     <div class="product_info">
-      <section>PERFUME</section>
-      <h1>Gabrielle Essence Eau De Parfum</h1>
-      <p>A floral, solar and voluptuous interpretation composed by Olivier Polge, 
-        Perfumer-Creator for the House of CHANEL.
-      </p>
-      <div class="price">
-      <span>$149.99</span>
-      <span class="discount">$169.99</span>
-      </div>
-    
-        <button class="add-to-cart">
-          <img src="/src/images/icon-cart.svg" alt="cart icon" />
-          Add to Cart
+     <main>
+        <section>PERFUME</section>
+        <h1>Gabrielle Essence Eau De Parfum</h1>
+
+        <p class="description">A floral, solar and voluptuous interpretation composed by Olivier Polge, 
+          Perfumer-Creator for the House of CHANEL.
+        </p>
+     </main>
+     <main>
+        <div class="price-container">
+          <span class="current-price">$149.99</span>
+          <span class="original-price">$169.99</span>
+        </div>
+      </main>
+      <button class="add-to-cart">
+        <img src="/src/images/icon-cart.svg" alt="cart icon"/>
+        <span>Add to Cart</span>
       </button>
-      
-    
     </div>
-    
-  </article>
+  </div>
   <div class="attribution">
    <p>
     Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-    Coded by <a href="#">Cornelio-II</a>.
+    Coded by <a href="#">Cornelio Jr</a>.
    </p>
   </div>
 </template>
 
 <style scoped>
+.product-container{
+  display: flex;
+  flex-direction: column;
+  background-color: var(--neutral-color-white);
+  border-radius: 0.625rem; 
+  overflow: hidden ;
+  margin: 1.5rem ;
+  max-width: 50em;
+}
 .product_info {
   padding: 1.5rem;
 }
 .product_info section {
   text-transform: uppercase;
-  font-weight: var(--montserrat-font-weight-medium);
   font-size: clamp(1rem, 0.956rem + 0.1878vw, 1.125rem);
   letter-spacing: 0.3rem;
   color: var(--neutral-color-gray);
 }
-
- .price  { 
+.price-container{
+  margin: 1rem 0;
+}
+.current-price { 
   color: var(--primary-color-500);
-  font-size: clamp(1.5rem, 0.75rem + 1.2vw, 2.2rem);
+  font-size: clamp(1.7rem, 1.5rem + 2vw, 2.3rem);
   font-weight: var(--montserrat-font-weight-bold);
   font-family: var(--fraunces-font);
-  
+  vertical-align: middle;
  }
- .discount{
+
+.original-price{
   text-decoration: line-through;
   color: var(--neutral-color-gray);
-  text-align: center;
-  justify-self: center;
-  margin-left: 1.8rem;
-  font-size: clamp(1rem, 0.5rem + 0.0939vw, 2rem);
-  font-weight: normal;
-
+  margin-left: 1rem;
+  vertical-align: middle;
 }
 .attribution {
   margin-top: 1rem;
   text-align: center;
- 
   color: var(--neutral-color-gray);
 }
 .attribution a {
@@ -78,32 +82,55 @@
 }
 .attribution a:hover {
   text-decoration: underline;
-  font-weight: var(--montserrat-font-weight-medium);
-}
-.add-to-cart{
-  display: flex;
-  
-  background-color: var(--primary-color-700);
-  color: var(--neutral-color-white);
-  align-items: center;
-  justify-content: center;
-  border: none;
-  height: 60px;  
-  width: 100%;
-  border-radius: 0.375rem;
-  padding: 0.75rem 1.5rem;
   font-weight: var(--montserrat-font-weight-bold);
-  font-size: 1rem;
+}
+.description{
+    font-size: clamp(1.125rem,  1rem + 0.1878vw, 2.5rem);
+    font-weight: var(--montserrat-font-weight-medium);
+ }
+.add-to-cart{
+  width: 100%;
+  height: auto;
+  background-color: var( --primary-color-500);
+  color: var(--neutral-color-white);
+  border: none;
+  outline: none;
+  border-radius: 0.625em;
+  padding: 0.75rem 1.5rem;
   cursor: pointer;
   transition: background-color 0.3s ease-in-out;
-  
-}
-.add-to-cart img{
-  width: 25px;
-  height: 30px;
-  margin-bottom: 10px;
+  margin: 1.2rem 0;
+  padding: 1.2rem;
+ 
 }
 .add-to-cart:hover {
-  background-color: var(--primary-color-500);
+  background-color: var(--primary-color-700);
 }
+
+.add-to-cart span{
+  font-size: 0.938rem;
+  vertical-align: middle;
+  font-weight: var(--montserrat-font-weight-bold);
+}
+
+.add-to-cart img {
+  margin-right: 1rem;
+  width: 1.2em;
+  height: 1.2em;
+  filter: brightness(0) invert(1);
+  vertical-align: middle;
+  transition: filter 0.3s ease-in-out;
+}
+
+@media (min-width: 37.5rem) {
+  .product-container{
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+  }
+  .product_info{
+    padding-inline: 2rem;
+  }
+ 
+}
+
 </style>
